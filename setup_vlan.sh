0727e14f981aee01986c1a21290fbaec2eeefa74
Alexey-Ershov <ershov_ae@mail.ru>
2018-08-29 17:07:11 +0300

    Add setup_vlan.sh script

---
+++ setup_vlan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $1 == "--help" || $1 == "-h" ]];
+then
+    echo "Usage: ./setup_vlan.sh <host_name> <vlan_tag> <IP-address>"
+else
+    HOST=$1
+    TAG=$2
+    IP=$3
+
+    modprobe 8021q
+
+    vconfig add $HOST-eth0 $TAG
+
+    ifconfig $HOST-eth0.$TAG up
+
+    ifconfig $HOST-eth0.$TAG $IP
+fi
